d20b3edf842895516a245fa8ed48d2384d6fad51
공채원 <chaewonkong@gmail.com>
2019-01-06 21:48:36 +0900

    Create basic structure

---
+++ client/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import styles from "../module_css/Footer.module.css";
+
+class Footer extends Component {
+  render() {
+    return <div className={styles.container}>This is Footer</div>;
+  }
+}
+
+export { Footer };
---
+++ client/src/components/PoemList.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import styles from "../module_css/PoemList.module.css";
+
+class PoemList extends Component {
+  render() {
+    return <div className={styles.mainContainer}>Poem List</div>;
+  }
+}
+
+export { PoemList };
