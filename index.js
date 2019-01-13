0031c912c4bc107d9fba7345134d9fd40dc5fbb8
공채원 <chaewonkong@gmail.com>
2019-01-13 15:11:49 +0900

    Add react build folder to routing

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const poems = require("./data/db.json");
+const path = require("path");
 
 const app = express();
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "client/build")));
+}
+
 app.get("/api/poems", (req, res) => {
   res.send(poems);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
 });
 
 const PORT = 5000 || process.env.PORT;
