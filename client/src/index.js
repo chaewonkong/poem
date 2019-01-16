5a00fb979dcf3b3cf35b794370fc72750c25bd68
공채원 <chaewonkong@gmail.com>
2019-01-16 11:55:03 +0900

    Add redux-thunk

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import reduxThunk from "redux-thunk";
 import { createStore, applyMiddleware } from "redux";
 
 import App from "./components/App";
 import reducers from "./reducers";
 import * as serviceWorker from "./serviceWorker";
 
-const store = createStore(reducers, {}, applyMiddleware());
+const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
