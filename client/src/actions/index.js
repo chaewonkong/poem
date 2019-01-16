ab7c97cf204a739e7672d41cac6809ce371406e5
공채원 <chaewonkong@gmail.com>
2019-01-16 11:57:14 +0900

    Refactorings to actions

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -3,5 +3,5 @@ import { FETCH_POEMS } from "./types";
 
 export const fetchPoems = () => async dispatch => {
   const res = await axios.get("/api/poems");
-  dispatch({ type: FETCH_POEMS, payload: res });
+  dispatch({ type: FETCH_POEMS, payload: res.data });
 };
