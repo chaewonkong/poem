c5dea63fd643c2906f28058c3e9a0fc4128375da
공채원 <chaewonkong@gmail.com>
2019-01-16 11:55:37 +0900

    add FETCH_POEMS case

--- client/src/reducers/poemReducer.js
+++ client/src/reducers/poemReducer.js
@@ -1,6 +1,12 @@
-export default function(state = {}, action) {
+import { FETCH_POEMS } from "../actions/types";
+export default function(
+  state = { poems: [{ title: "", id: "", content: "" }] },
+  action
+) {
   console.log(action);
   switch (action.type) {
+    case FETCH_POEMS:
+      return action.payload;
     default:
       return state;
   }
