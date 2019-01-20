46049b316a64c5379f910eb0125f057f7c31debe
공채원 <chaewonkong@gmail.com>
2019-01-20 18:37:53 +0900

    Update PoemCard css

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,16 @@
 import React, { Component } from "react";
+import Button from "@material-ui/core/Button";
 import styles from "../css/Footer.module.css";
 
 class Footer extends Component {
   render() {
-    return <div className={styles.container}>This is Footer</div>;
+    return (
+      <div className={styles.container}>
+        <Button>시</Button>
+        <Button>내 정보</Button>
+        <Button>더 보기</Button>
+      </div>
+    );
   }
 }
 
--- client/src/components/PoemCard.js
+++ client/src/components/PoemCard.js
@@ -4,11 +4,18 @@ import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import CardHeader from "@material-ui/core/CardHeader";
+import Avatar from "@material-ui/core/Avatar";
 
 const PoemCard = props => {
   return (
-    <Card>
-      <CardContent>
+    <Card style={{ marginBottom: "2vh" }}>
+      <CardHeader
+        avatar={<Avatar aria-label="Recipe">R</Avatar>}
+        title="Shrimp and Chorizo Paella"
+        subheader="September 14, 2016"
+      />
+      <CardContent style={{ textAlign: "center" }}>
         <Typography variant="h5" color="textPrimary" gutterBottom>
           {props.title}
         </Typography>
@@ -20,7 +27,7 @@ const PoemCard = props => {
       </CardContent>
       <div>
         <Button size="small" variant="flat">
-          Learn More
+          좋아요
         </Button>
       </div>
     </Card>
