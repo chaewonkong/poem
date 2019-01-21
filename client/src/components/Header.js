650028e6cbd2103bb10127b2f381f8de98b1c2fb
공채원 <chaewonkong@gmail.com>
2019-01-21 13:33:45 +0900

    Change Header to AppBar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,26 +1,45 @@
 import React, { Component } from "react";
 import Typography from "@material-ui/core/Typography";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 import { Link } from "react-router-dom";
 import "../css/Header.css";
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        <nav>
-          <div className="nav-wrapper">
+      <AppBar position="fixed" color="#fff">
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center"
+          }}
+        >
+          <Toolbar>
+            <IconButton color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
             <Link
               to="/"
               className="brand-logo"
               style={{ textDecoration: "none" }}
             >
-              <Typography color="secondary" variant="h5">
+              <Typography variant="h6" color="secondary">
                 하루시作
               </Typography>
             </Link>
-          </div>
-        </nav>
-      </div>
+            <Link
+              to="/login"
+              style={{ color: "black", textDecoration: "none" }}
+            >
+              <Button color="inherit">Login</Button>
+            </Link>
+          </Toolbar>
+        </div>
+      </AppBar>
     );
   }
 }
