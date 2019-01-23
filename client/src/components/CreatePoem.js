6424464a0e98ef8fa55ef31761effad60f74cf9f
공채원 <chaewonkong@gmail.com>
2019-01-23 12:07:36 +0900

    Add auth before creating poem

--- client/src/components/CreatePoem.js
+++ client/src/components/CreatePoem.js
@@ -14,6 +14,74 @@ class CreatePoem extends Component {
     content: ""
   };
 
+  renderForm = () => {
+    console.log(this.props.auth.token);
+    if (this.props.auth.token === undefined) {
+      return <Redirect push to="/login" />;
+    } else if (this.props.poems.post_success) {
+      return <Redirect push to="/" />;
+    } else {
+      return (
+        <div>
+          <PoemSubject title="꽃" />
+          <form>
+            <TextField
+              id="title"
+              name="title"
+              fullWidth
+              label="시 제목"
+              multiline
+              margin="normal"
+              variant="outlined"
+              placeholder="서시"
+              onChange={this.handleChange}
+              value={this.state.title}
+            />
+            <TextField
+              id="content"
+              name="content"
+              className="asdf"
+              fullWidth
+              label="시 내용"
+              multiline
+              margin="normal"
+              variant="outlined"
+              rows="10"
+              onChange={this.handleChange}
+              value={this.state.content}
+              placeholder={`
+          죽는 날까지 하늘을 우러러
+          한점 부끄럼이 없기를
+          잎새에 이는 바람에도
+          나는 괴로워했다
+          별을 노래하는 마음으로
+          모든 죽어가는 것을 사랑해야지
+          그리고 나한테 주어진 길을
+          걸어가야겠다.
+          
+          오늘 밤에도 별이 바람에 스치운다.`}
+            />
+          </form>
+          <div className={styles.btnCreate}>
+            <Button
+              type="submit"
+              onClick={this.handleSubmit}
+              color="primary"
+              variant="outlined"
+            >
+              작성완료
+            </Button>
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <Button color="secondary" variant="outlined">
+                작성취소
+              </Button>
+            </Link>
+          </div>
+        </div>
+      );
+    }
+  };
+
   handleChange = e => {
     this.setState({
       [e.target.name]: e.target.value,
@@ -32,70 +100,7 @@ class CreatePoem extends Component {
     );
   };
   render() {
-    return (
-      <div>
-        {this.props.poems.post_success ? (
-          <Redirect push to="/" />
-        ) : (
-          <div>
-            <PoemSubject title="꽃" />
-            <form>
-              <TextField
-                id="title"
-                name="title"
-                fullWidth
-                label="시 제목"
-                multiline
-                margin="normal"
-                variant="outlined"
-                placeholder="서시"
-                onChange={this.handleChange}
-                value={this.state.title}
-              />
-              <TextField
-                id="content"
-                name="content"
-                className="asdf"
-                fullWidth
-                label="시 내용"
-                multiline
-                margin="normal"
-                variant="outlined"
-                rows="10"
-                onChange={this.handleChange}
-                value={this.state.content}
-                placeholder={`
-          죽는 날까지 하늘을 우러러
-          한점 부끄럼이 없기를
-          잎새에 이는 바람에도
-          나는 괴로워했다
-          별을 노래하는 마음으로
-          모든 죽어가는 것을 사랑해야지
-          그리고 나한테 주어진 길을
-          걸어가야겠다.
-          
-          오늘 밤에도 별이 바람에 스치운다.`}
-              />
-            </form>
-            <div className={styles.btnCreate}>
-              <Button
-                type="submit"
-                onClick={this.handleSubmit}
-                color="primary"
-                variant="outlined"
-              >
-                작성완료
-              </Button>
-              <Link to="/" style={{ textDecoration: "none" }}>
-                <Button color="secondary" variant="outlined">
-                  작성취소
-                </Button>
-              </Link>
-            </div>
-          </div>
-        )}
-      </div>
-    );
+    return <div>{this.renderForm()}</div>;
   }
 }
 const mapStateToProps = state => {
