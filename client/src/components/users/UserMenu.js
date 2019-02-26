32dcac438b10ff5f04b6e6be3c191333ac476256
공채원 <chaewonkong@gmail.com>
2019-02-26 16:52:26 +0900

    Change drawer background

--- client/src/components/users/UserMenu.js
+++ client/src/components/users/UserMenu.js
@@ -6,6 +6,7 @@ import MenuIcon from "@material-ui/icons/Menu";
 import Typography from "@material-ui/core/Typography";
 import * as actions from "../../actions";
 import { Dropdown, Button } from "antd";
+import "../../css/UserMenu.css";
 
 class UserMenu extends Component {
   state = { visible: false, placement: "left" };
@@ -61,23 +62,14 @@ class UserMenu extends Component {
           onClose={this.onClose}
           visible={this.state.visible}
         >
-          {" "}
           <Dropdown overlay={menu} placement="bottomCenter" trigger={["click"]}>
-            {/* <Button> */}
             <div style={{ display: "flex", alignItems: "center" }}>
               <Avatar src={this.props.auth.image} />
               <Typography variant="h6" style={{ color: "#A4A4A4" }}>
                 {this.props.auth.nickname}
               </Typography>
             </div>
-            {/* </Button> */}
           </Dropdown>
-          {/* <div style={{ display: "flex", alignItems: "center" }}>
-              <Avatar src={this.props.auth.image} />
-              <Typography variant="h6" style={{ color: "#A4A4A4" }}>
-                {this.props.auth.nickname}
-              </Typography>
-            </div> */}
           <br />
           <div
             style={{
@@ -88,7 +80,7 @@ class UserMenu extends Component {
           >
             <img
               alt="모아보기"
-              src="https://s3.ap-northeast-2.amazonaws.com/harusijak-static-manage/static_image/%E1%84%86%E1%85%A9%E1%84%8B%E1%85%A1%E1%84%87%E1%85%A9%E1%84%80%E1%85%B5+%E1%84%8B%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%8F%E1%85%A9%E1%86%AB.svg"
+              src="https://s3.ap-northeast-2.amazonaws.com/harusijak-static-manage/static_image/%EB%AA%A8%EC%95%84%EB%B3%B4%EA%B8%B0+%EC%95%84%EC%9D%B4%EC%BD%98.svg"
             />
             <p>모아보기</p>
           </div>
@@ -114,7 +106,7 @@ class UserMenu extends Component {
           >
             <img
               alt="담아온 시"
-              src="https://s3.ap-northeast-2.amazonaws.com/harusijak-static-manage/static_image/%E1%84%83%E1%85%A1%E1%86%B7%E1%84%8B%E1%85%A1%E1%84%8B%E1%85%A9%E1%86%AB%E1%84%89%E1%85%B5+%E1%84%8B%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%8F%E1%85%A9%E1%86%AB.svg"
+              src="https://s3.ap-northeast-2.amazonaws.com/harusijak-static-manage/static_image/%EB%8B%B4%EC%95%84%EA%B0%80%EA%B8%B0%EC%95%84%EC%9D%B4%EC%BD%98.svg"
             />
             <p>담아온 시</p>
           </div>
@@ -127,7 +119,7 @@ class UserMenu extends Component {
           >
             <img
               alt="설정"
-              src="https://s3.ap-northeast-2.amazonaws.com/harusijak-static-manage/static_image/%E1%84%89%E1%85%A5%E1%86%AF%E1%84%8C%E1%85%A5%E1%86%BC+%E1%84%8B%E1%85%A1%E1%84%8B%E1%85%B5%E1%84%8F%E1%85%A9%E1%86%AB.svg"
+              src="https://s3.ap-northeast-2.amazonaws.com/harusijak-static-manage/static_image/%EC%84%A4%EC%A0%95+%EC%95%84%EC%9D%B4%EC%BD%98.svg"
             />
             <p>설정</p>
           </div>
