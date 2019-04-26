dd41cce0cc42c3df7396b82aa20d6ac6d1ccaf3f
공채원 <chaewonkong@gmail.com>
2019-04-26 18:06:55 +0900

    refactor: separate presenter from container
    mkdir UserMenu.
    move Usermenu to Usermenu/

--- client/src/components/common/DefaultHeader.js
+++ client/src/components/common/DefaultHeader.js
@@ -7,7 +7,7 @@ import Button from "@material-ui/core/Button";
 import Modal from "@material-ui/core/Modal";
 import styled from "styled-components";
 import * as actions from "../../actions";
-import UserMenu from "../users/UserMenu";
+import UserMenu from "../users/UserMenu/";
 import { Search, CustomHeader } from "./";
 import { media, color } from "../../config/_mixin";
 
